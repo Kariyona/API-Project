58aadbf9bdc6c85ba9e04997082a3498e249bbc2
Wyona <yanyan@Wyonas-MacBook-Pro.local>
2023-03-26 23:23:09 -0700

    fixed foreignkey spelling

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Booking.belongsTo(models.User, { foreignKey: "userId" });
-      Booking.belongsTo(models.Spot, { foreignkeY: "spotId" });
+      Booking.belongsTo(models.Spot, { foreignKey: "spotId" });
     }
   }
   Booking.init(
