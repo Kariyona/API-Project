898a40e4a3021d92957b932f3a52e5c12e44073b
Wyona <yanyan@Wyonas-MacBook-Pro.local>
2023-04-10 17:06:45 -0700

    seeders changed once again

--- backend/db/seeders/20230321220122-seed-spots.js
+++ backend/db/seeders/20230321220122-seed-spots.js
@@ -20,7 +20,7 @@ module.exports = {
           country: "USA",
           lat: 34.0922, //-90 90
           lng: -118.5023, //-180 180
-          name: "Ocean Front with Pool on Wild Sonoma Coast",
+          name: "Cozy Cabin in the Wild Sonoma Coast",
           description: "The Cove House in Timber Cove offers a dramatic and romantic experience you will savor for a lifetime. Intimate enough for the honeymoon of your dreams yet spacious enough for two couples. White water forever views, large spa and bluff top pool.",
           price: 620,
         },
@@ -75,13 +75,13 @@ module.exports = {
         {
           ownerId: 3,
           address: "15 Park Ave.",
-          city: "New York",
-          state: "New York",
-          country: "USA",
+          city: "Vancouver",
+          state: "Canada",
+          country: "B.C.",
           lat: 41.3692, //-90 90
           lng: -87.9211, //-180 180
           name: "Cabin Under The Night Sky ",
-          description: "Best view of the night sky and stars in New York. You won't get this anywhere in the city!",
+          description: "Best view of the night sky and stars in Canada. Many drive up from Washington.",
           price: 640,
         },
         {
@@ -94,7 +94,7 @@ module.exports = {
           lng: -73.9291, //-180 180
           name: "Spacious Condo with Golden Gate Bridge view",
           description: "Open floor plan with lots of natural lighting. Away from the smelly street.",
-          price: 520,
+          price: 150,
         },
         {
           ownerId: 2,
@@ -106,7 +106,7 @@ module.exports = {
           lng: 103.9084, //-180 180
           name: "Beautiful home in heart of downtown Chicago",
           description: "Located in the heart of downtown, it is walking distance everywhere.",
-          price: 300,
+          price: 325,
         },
         {
           ownerId: 3,
@@ -118,7 +118,7 @@ module.exports = {
           lng: 115.1772, //-180 180
           name: "Beachfront Bungalow Villa",
           description: "Bring your friends and unwind with the nicest views in Arizona. Escape the heat.",
-          price: 200,
+          price: 382,
         },
         {
           ownerId: 1,
@@ -130,7 +130,7 @@ module.exports = {
           lng: 2.1347, //-180 180
           name: "Cherry Villa",
           description: "Our dog friendly villa is located 1/2 block from the Lake! 1/2 block to Patton Beach-the only dog friendly beach in the North Shore, & GarWoods Restaurant.",
-          price: 400,
+          price: 412,
         },
       ],
       {}
